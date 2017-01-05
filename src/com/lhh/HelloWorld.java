4a6c7233099c091364cefb300ca2e9bb8b380959
maktubliu <lqlchh@163.com>
2017-01-05 09:54:39 +0800

    HelloWorld

--- src/com/lhh/HelloWorld.java
+++ src/com/lhh/HelloWorld.java
@@ -1,9 +1,9 @@
 package com.lhh;
 
-public class Main {
+public class HelloWorld {
 
     public static void main(String[] args) {
 	// write your code here
-
+        System.out.println("Hello world!");
     }
 }
