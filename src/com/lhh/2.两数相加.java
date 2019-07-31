4a0e2ebbde3082fc8ad0e3fc19a4c33e0494df28
maktubliu <lqlchh@163.com>
2019-07-31 15:34:53 +0800

    2

--- src/com/lhh/2.两数相加.java
+++ src/com/lhh/2.两数相加.java
@@ -4,16 +4,43 @@
  * [2] 两数相加
  */
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
  */
+//
+//public class ListNode {
+//    int val;
+//    ListNode next;
+//
+//    ListNode(int x) {
+//        val = x;
+//    }
+//}
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
+        ListNode pre = new ListNode(0);
+        ListNode curr = pre;
+        int carry = 0;
+        while(l1 != null || l2 != null){
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            curr.next = new ListNode(sum);
+            curr = curr.next;
+            if(l1 != null){
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                l2 = l2.next;
+            }
+
+        }
+        if(carry == 1){
+            curr.next = new ListNode(carry);
+        }
+        return pre.next;
     }
 }
-
