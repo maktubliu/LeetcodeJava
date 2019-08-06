1c040d0d2462c3ebf11193fdcea55acb28c9eb88
maktubliu <lqlchh@163.com>
2019-08-06 12:10:08 +0800

    387

--- src/com/lhh/387.字符串中的第一个唯一字符.java
+++ src/com/lhh/387.字符串中的第一个唯一字符.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * @lc app=leetcode.cn id=387 lang=java
  *
@@ -5,14 +8,17 @@
  */
 class Solution {
     public int firstUniqChar(String s) {
-       int index = -1;
-       for(char ch = 'a'; ch <= 'z'; ch++){
-           int beginIndex = s.indexOf(ch);
-           if(beginIndex != -1 && beginIndex == s.lastIndexOf(ch)){
-               index = (index == -1 || index > beginIndex) ? beginIndex : index;
-           }
-       }
-       return index;
+        int n = s.length();
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for(int i = 0; i < n; i++){
+            if(map.get(s.charAt(i)) == 1){
+                return i;
+            }
+        }
+        return -1;
+    
     }
 }
-
